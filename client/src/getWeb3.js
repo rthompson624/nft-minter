8d41b393ccad58fc7b2fb2c562f983fea7505393
Richard Thompson <rthompson624@gmail.com>
2022-02-17 13:21:40 -0500

    Fix window.ethereum.enable() warning

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -6,11 +6,9 @@ const getWeb3 = () =>
     window.addEventListener("load", async () => {
       // Modern dapp browsers...
       if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
         try {
-          // Request account access if needed
-          await window.ethereum.enable();
-          // Accounts now exposed
+          await window.ethereum.request({ method: 'eth_requestAccounts' });
+          const web3 = new Web3(window.ethereum);
           resolve(web3);
         } catch (error) {
           reject(error);
