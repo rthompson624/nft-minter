89449528c1e09b3eeb0ba2366729e1ba5bffe7a9
Richard Thompson <rthompson624@gmail.com>
2022-02-17 13:49:43 -0500

    Small fix

--- client/src/components/ViewableCount.js
+++ client/src/components/ViewableCount.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 export default function ViewableCount({ nftRecords }) {
+  const units = nftRecords.length === 1 ? 'item' : 'items';
   return (
-    <div className="font-mono">{nftRecords.length} items</div>
+    <div className="font-mono">{ `${nftRecords.length} ${units}` }</div>
   );
 }
