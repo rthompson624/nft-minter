c6082e83faa16aad651af5abbdb099c52bbeb29f
Richard Thompson <rthompson624@gmail.com>
2022-02-23 18:32:11 -0500

    Fix z-order of loading indicator

--- client/src/components/LoadingIndicator.js
+++ client/src/components/LoadingIndicator.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function LoadingIndicator({ loading }) {
   if (loading) {
     return (
-      <div className="h-1 bg-blue-500 sticky top-0 animate-pulse"></div>
+      <div className="z-10 h-1 bg-blue-500 sticky top-0 animate-pulse"></div>
     );
   }
   return (
-    <div className="h-1 bg-white sticky top-0"></div>
+    <div className="z-10 h-1 bg-white sticky top-0"></div>
   );
 }
